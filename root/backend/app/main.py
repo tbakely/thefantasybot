b53099b009c6cb2b22055de40c330c12ad0032f6
Tyler Bakely <tylerbakely@Tylers-MBP.fios-router.home>
2023-02-22 01:38:34 -0500

    Added logic to connect to postgres

--- root/backend/app/main.py
+++ root/backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routers import players
+import psycopg2
 
 app = FastAPI()
 
--- root/backend/app/routers/players.py
+++ root/backend/app/routers/players.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI, APIRouter
+import psycopg2
 
+while True:
+    try:
+        conn = psycopg2.connect("dbname=thefantasybot user=tbakely")
+        cursor = conn.cursor()
+        print("Connection successful.")
+        break
+    except Exception as error:
+        print("Connection failed.")
+        print("Error: ", error)
 
 router = APIRouter(prefix="/players", tags=["players"])
 
 
 @router.get("/")
 async def get_players():
-    return {"message": "Checked!"}
+    cursor.execute("""SELECT * FROM players""")
+    records = cursor.fetchall()
+    return {"data": records}
