aaaa7876c852bdff2767cdf50db4c30e0b09dea6
Tyler Bakely <tylerbakely@icloud.com>
2023-03-05 23:23:00 -0500

    Added initial frontend packages from Vue and started building sample code for API

---
+++ root/frontend/thefantasybot-frontend/src/App.vue
@@ -0,0 +1,71 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<script>
+import ViewPlayers from './components/ViewPlayers.vue';
+
+export default {
+  components: {
+    ViewPlayers
+  },
+  data() {
+    return {
+      players: [
+        {
+          id: 1,
+          player: "James Jamesington",
+          position: "RB",
+          value_score: 23,
+          adp: 13,
+          sleeper: 7
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
