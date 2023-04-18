f531806321deb8456b2a8550e55caae1edcfca3b
Tyler Bakely <tylerbakely@icloud.com>
2023-04-18 01:28:44 -0400

    Changed backend to read in json files.

--- root/backend/app/routers/players.py
+++ root/backend/app/routers/players.py
@@ -1,33 +1,37 @@
 from fastapi import FastAPI, APIRouter
 from app.schemas import Player
-import psycopg2
+import pandas as pd
+import os
+import json
 
 while True:
     try:
-        conn = psycopg2.connect("dbname=thefantasybot user=tbakely")
-        cursor = conn.cursor()
-        print("Connection successful.")
+        file_path = (
+            "/Users/tylerbakely/Desktop/repos/thefantasybot/root/backend/app/data/"
+        )
+        with open(os.path.join(file_path, "draftboard_standard.json"), "r") as f:
+            data = json.load(f)
+        print("Player data loaded successfully.")
         break
     except Exception as error:
-        print("Connection failed.")
+        print("Could not load player data.")
         print("Error: ", error)
+        break
 
 router = APIRouter(prefix="/players", tags=["players"])
 
 
-@router.get("/")
+@router.get("/", response_model=list[Player])
 async def get_players():
-    cursor.execute("""SELECT * FROM players""")
-    records = cursor.fetchall()
     results = [
         Player(
-            id=i[0],
-            player=i[1],
-            position=i[2],
-            value_score=i[3],
-            adp=i[4],
-            sleeper_score=i[5],
+            id=id,
+            player=player["PLAYER"],
+            position=player["POS"],
+            value_score=player["VOR Rank"],
+            adp=player["ADP Rank"],
+            sleeper_score=player["Sleeper Score"],
         )
-        for i in records
+        for id, player in enumerate(data)
     ]
     return results
